8550bb5fffd46fd70bc0d171e553dc8811e77fb1
phoebe <anlica62712@gmail.com>
2023-11-13 22:16:35 +0800

    feat: add message

--- src/main/java/com/example/product/exception/ServiceException.java
+++ src/main/java/com/example/product/exception/ServiceException.java
@@ -1,7 +1,10 @@
 package com.example.product.exception;
 
+import java.io.Serial;
+
 public class ServiceException extends RuntimeException{
 
+    @Serial
     private static final long serialVersionUID = -4513514459698548384L;
 
     public ServiceException(String message) {
--- src/main/java/com/example/product/service/ProductService.java
+++ src/main/java/com/example/product/service/ProductService.java
@@ -12,7 +12,6 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
