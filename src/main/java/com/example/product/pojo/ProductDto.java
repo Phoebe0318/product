187eb59bba123eee87dba1f808255880be41c408
phoebe <anlica62712@gmail.com>
2023-11-14 21:59:38 +0800

    fix: alter ProductDto field name

--- src/main/java/com/example/product/pojo/ProductDto.java
+++ src/main/java/com/example/product/pojo/ProductDto.java
@@ -11,7 +11,7 @@ import java.math.BigDecimal;
 @Schema(name = "ProductDto", description = "Product information")
 public class ProductDto {
     @Schema(description = "Name of the product", example = "Smartphone")
-    private String name;
+    private String title;
 
     @Schema(description = "Description of the product")
     private String description;
